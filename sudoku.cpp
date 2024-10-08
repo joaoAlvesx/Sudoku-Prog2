b3563822b03f9e21a396e41808da745a2f0a1c23
joaoAlvesx <alvesjoaovitor463@gmail.com>
2024-10-08 18:17:16 -0400

    adicionando docs

--- sudoku.cpp
+++ sudoku.cpp
@@ -69,28 +69,22 @@ FILE* carregue(char quadro[9][9]) {
 
 		// carregar novo sudoku
 		case 1:
-			break;
+			printf("Qual o nome do arquivo txt");
+			carregue_novo_jogo()
 
 		// continuar jogo
 		case 2:
-			break;
+			printf("Qual o nome do arquivo binario");
 
 		// retornar ao menu anterior
 		case 9:
-			break;
+			return;
 
 		default:
 			break;
 }
 
-/* -----------------------------------------------------------------------------
- * CARREGAR CONTINUAR JOGO
- * Carrega um estado de jogo a partir de um arquivo binario
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
-FILE* carregue_continue_jogo (char quadro[9][9], char *nome_arquivo) {
-	// TODO
-}
+
 
 /* -----------------------------------------------------------------------------
  * CARREGAR NOVO JOGO
@@ -100,8 +94,12 @@ FILE* carregue_continue_jogo (char quadro[9][9], char *nome_arquivo) {
 void carregue_novo_jogo(char quadro[9][9], char *nome_arquivo) {
 	// TODO
 }
-
 /* -----------------------------------------------------------------------------
+ * CARREGAR CONTINUAR JOGO
+ * Carrega um estado de jogo a partir de um arquivo binario
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+ /* -----------------------------------------------------------------------------
  * CRIAR ARQUIVO BINARIO
  * Criar arquivo binario
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -109,6 +107,12 @@ void carregue_novo_jogo(char quadro[9][9], char *nome_arquivo) {
 FILE* crie_arquivo_binario(char quadro[9][9]) {
 	// TODO
 }
+
+FILE* carregue_continue_jogo (char quadro[9][9], char *nome_arquivo) {
+	// TODO
+}
+
+
 
 /* -----------------------------------------------------------------------------
  * DETERMINAR QUADRANTE
