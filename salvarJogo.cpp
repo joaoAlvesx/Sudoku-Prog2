5b734d5f624c6f71b24dccc037a84624459b964c
joaoAlvesx <alvesjoaovitor463@gmail.com>
2024-10-11 11:06:39 -0400

    codigos a serem feitos separados por arquivo

---
+++ salvarJogo.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* -----------------------------------------------------------------------------
+ * SALVAR JOGADA BINARIO
+ * Salva o estado atual do quadro no arquivo binario
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+void salve_jogada_bin (FILE *fb, char quadro[9][9]) {
+	// TODO
+}
