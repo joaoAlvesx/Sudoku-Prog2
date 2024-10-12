261ce5bfb932401f75d0ab6a0fdee1a0499eac0d
joaoAlvesx <alvesjoaovitor463@gmail.com>
2024-10-12 10:18:52 -0400

    comecando a codar

--- codigos/carregue.cpp
+++ codigos/carregue.cpp
@@ -1,32 +1,76 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-FILE* carregue(char quadro[9][9]) {
-	int opcao;
+
+void carregue(char quadro[9][9]);
+FILE* carregue_continue_jogo (char quadro[9][9], char *nome_arquivo);
+void carregue_novo_jogo(char quadro[9][9], char *nome_arquivo);
+FILE* crie_arquivo_binario(char quadro[9][9]);
+
+
+/* Funcoes auxiliares */
+void imprima(const char quadro[9][9]);
+int leia_opcao();
+void menu();
+void menu_arquivo();
+
+
+int main(){
+
+	char quadro[9][9] = { {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+	FILE *fb = NULL;
+
+	int opcao = 0;
+	imprima(quadro);
+
+	// apresente um menu com as opcoes
+	menu();
+	opcao = leia_opcao();
+	if (opcao == 1){
+	carregue(quadro);
+
+	if (fb == NULL) {
+		fb = crie_arquivo_binario(quadro);
+	}
+
+	}
+}
+
+
+void carregue(char quadro[9][9]) {
+	int opcao = 0;
 
 	menu_arquivo();
+	printf("aqui");
 	opcao = leia_opcao();
-
+	printf("chegou no if");
 	// TODO Função incompleta
 
-	switch(opcao) {
-
-		// carregar novo sudoku
-		case 1:
-			printf("Qual o nome do arquivo txt");
-			carregue_novo_jogo()
-
+	if (opcao == 1)
+	{
+		printf("Qual o nome do arquivo txt");
+		
+	}else if(opcao == 2)
+	{
 		// continuar jogo
-		case 2:
 			printf("Qual o nome do arquivo binario");
+	}/*else{
+		return 0;
+	}*/
 
-		// retornar ao menu anterior
-		case 9:
-			return;
+		
 
-		default:
-			break;
 }
+
 /* -----------------------------------------------------------------------------
  * CARREGAR NOVO JOGO
  * Carrega um novo jogo do Sudoku
@@ -34,6 +78,7 @@ FILE* carregue(char quadro[9][9]) {
  */
 void carregue_novo_jogo(char quadro[9][9], char *nome_arquivo) {
 	// TODO
+	printf("carregue novo jogo");
 }
 /* -----------------------------------------------------------------------------
  * CARREGAR CONTINUAR JOGO
@@ -47,9 +92,73 @@ void carregue_novo_jogo(char quadro[9][9], char *nome_arquivo) {
  */
 FILE* crie_arquivo_binario(char quadro[9][9]) {
 	// TODO
+	printf("carregue novo jogo");
 }
 
 FILE* carregue_continue_jogo (char quadro[9][9], char *nome_arquivo) {
+	printf("carregue novo jogo");
 	// TODO
 }
 
+
+int leia_opcao () {
+	int opcao = 0;
+
+	printf("Opcao: ");
+	scanf("%d", &opcao);
+	printf("dentro da funcao");
+	return opcao;
+}
+
+/* -----------------------------------------------------------------------------
+ * MENU
+ * Imprime o menu de opcoes
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+void menu() {
+	puts("\n~~~~~~~~ SUDOKU ~~~~~~~~");
+	puts("[1] - Carregar jogo");
+	puts("[2] - Jogar");
+	puts("[3] - Resolver um passo");
+	puts("[4] - Resolver");
+	puts("[9] - Finalizar");
+	puts("--------");
+}
+
+/* -----------------------------------------------------------------------------
+ * MENU ARQUIVO
+ * Imprime o menu de opcoes do arquivo
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+void menu_arquivo() {
+	puts("\n~~~~~ MENU ARQUIVO ~~~~~");
+	puts("[1] - Novo jogo");
+	puts("[2] - Continuar jogo");
+	puts("[9] - Retornar ao menu anterior");
+	puts("--------");
+}
+
+void imprima(const char quadro[9][9]) {
+	int i, j;
+
+//	puts("~~~~~~~~ SUDOKU ~~~~~~~~");
+	puts("    1 2 3   4 5 6   7 8 9");
+	for (i = 0; i < 9; i++) {
+		if (i % 3 == 0)
+			puts("  -------------------------");
+		for (j = 0; j < 9; j++) {
+
+			if (j == 0)
+				printf("%d | ", i+1);
+			else if (j % 3 == 0)
+				printf("| ");
+
+			if (quadro[i][j] == 0)
+				printf("- ");
+			else
+				printf("%d ", quadro[i][j]);
+		}
+		puts("|");
+	}
+	puts("  -------------------------");
+}
